f98b925f60c0d98997087fec8342436b507db35b
RedaEssawy <redaessawy81@gmail.com>
2025-06-15 02:56:24 +0300

    api files

---
+++ lib/core/api/dio_consumer.dart
@@ -0,0 +1,83 @@
+import 'package:dio/dio.dart';
+import 'package:smart_home/core/api/api_consumer.dart';
+import 'package:smart_home/core/api/api_interceptors.dart';
+import 'package:smart_home/core/errors/exceptions.dart';
+
+class DioConsumer extends ApiConsumer {
+  final Dio dio;
+
+  DioConsumer({required this.dio}) {
+    dio.options.baseUrl = '';
+    dio.interceptors.add(ApiInterceptors());
+    dio.interceptors.add(LogInterceptor(
+      responseBody: true,
+      requestBody: true,
+      error: true,
+      requestHeader: false,
+      responseHeader: false,
+    ));
+  }
+  @override
+  Future delete(String path,
+      {Object? data,
+      Map<String, dynamic>? queryParameters,
+      bool isFormData = false}) async {
+    // TODO: implement delete
+    try {
+      final response = await dio.delete(path,
+          data: isFormData
+              ? FormData.fromMap(data as Map<String, dynamic>)
+              : data,
+          queryParameters: queryParameters);
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+
+  @override
+  Future get(String path,
+      {Object? data, Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response =
+          await dio.get(path, data: data, queryParameters: queryParameters);
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+
+  @override
+  Future post(String path,
+      {Object? data,
+      Map<String, dynamic>? queryParameters,
+      bool isFormData = false}) async {
+    try {
+      final response = await dio.post(path,
+          data: isFormData
+              ? FormData.fromMap(data as Map<String, dynamic>)
+              : data,
+          queryParameters: queryParameters);
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+
+  @override
+  Future put(String path,
+      {Object? data,
+      Map<String, dynamic>? queryParameters,
+      bool isFormData = false}) async {
+    try {
+      final response = await dio.put(path,
+          data: isFormData
+              ? FormData.fromMap(data as Map<String, dynamic>)
+              : data,
+          queryParameters: queryParameters);
+      return response.data;
+    } on DioException catch (e) {
+      handleDioExceptions(e);
+    }
+  }
+}
---
+++ lib/core/api/end_points.dart
@@ -0,0 +1,14 @@
+class EndPoints {
+  static String baseUrl = 'http://38.0.101.76:3000';
+  static String singIn = 'http://38.0.101.76:3000';
+  static String singUp = 'http://38.0.101.76:3000';
+}
+
+class ApiKey {
+  static String status = 'status';
+  static String errorMessage = 'ErrorMessage';
+  static String email = 'email';
+  static String password = 'password';
+  static String token = 'token';
+  static String id = 'id';
+}
