3aff5159f3ff74e1df1e40b7cc7ac59edcce5aeb
RedaEssawy <redaessawy81@gmail.com>
2025-06-18 00:37:54 +0300

    app colors file

---
+++ lib/core/util/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color grey = Colors.grey;
+  static Color grey2 = Colors.grey.shade200;
+  static Color grey3 = Colors.grey.shade300;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color blue = Colors.blue;
+  static Color lightBlue = Colors.blue.shade100;
+  static const Color red = Colors.red;
+  static const Color green = Colors.green;
+  static const Color yellow = Colors.yellow;
+  static const Color deepPrple = Colors.deepPurple;
+  static const Color black45 = Colors.black45;
+}
