e185eabb67222ecf97a0e85d4a23b5130cf9287d
RedaEssawy <redaessawy81@gmail.com>
2025-06-18 00:41:24 +0300

    route pages

---
+++ lib/core/util/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:smart_home/core/pages/login_page.dart';
+import 'package:smart_home/core/pages/nav_bottom_page.dart';
+import 'package:smart_home/core/util/app_routes.dart';
+import 'package:smart_home/features/home/home.dart';
+
+import '../pages/register_page.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginPage());
+      case AppRoutes.registerRoute:
+        return MaterialPageRoute(builder:(_)=>const RegisterPage());
+      case AppRoutes.dashboardRoute:
+        return MaterialPageRoute(builder: (_)=> const NavBottomPage());
+      default:
+        return MaterialPageRoute(builder: (_) => const Home());
+    }
+  }
+}
---
+++ lib/core/util/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String loginRoute = '/login';
+  static const String registerRoute = '/register';
+  static const String homeRoute = '/home';
+  static const String dashboardRoute = '/dashboard';
+  
+}
